0c45d5d03758f3525683d4afb28b4c34e16c9140
Dash2 <sukdamrong.k@gmail.com>
2018-12-27 10:15:19 +0700

    fix Logformat Newline and Add comment

--- internal/helper.go
+++ internal/helper.go
@@ -1,11 +1,12 @@
 package internal
 
+//LogFormat is method middleware.LoggerConfig()
 func LogFormat() string {
 	// Refer to https://github.com/tkuchiki/alp
 	var format string
 	format += "host:${host}\t"
 	format += "uri:${uri}\t"
 	format += "method:${method}\t"
-	format += "status:${status}\t"
+	format += "status:${status}\n"
 	return format
 }
